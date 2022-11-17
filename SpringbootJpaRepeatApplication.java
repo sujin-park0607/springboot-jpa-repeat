7e068468d244c59262415b65a68fba95032838ed
sujin-park0607 <goweg0607@gmail.com>
2022-11-17 22:52:04 +0900

    feat: setup

---
+++ SpringbootJpaRepeatApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootjparepeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaRepeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJpaRepeatApplication.class, args);
+	}
+
+}
